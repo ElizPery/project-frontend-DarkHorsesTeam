7fc23448acad5f90b38acf8714888411f61478d4
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-10-18 16:00:29 +0300

    add future signin page

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,11 +1,25 @@
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-axios.defaults.baseURL = "";
+axios.defaults.baseURL = '';
 
-// const setAuthHeader = (token) => {
-//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 // const clearAuthHeader = () => {
 //   axios.defaults.headers.common.Authorization = '';
 // };
+
+export const logIn = createAsyncThunk(
+  'auth/logIn',
+  async (userData, thunkAPI) => {
+    try {
+      const response = await axios.post('auth/login', userData);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,48 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import { logIn } from './operations.js';
+import toast from 'react-hot-toast';
 
 const initialState = {
-    user: {
-        name: null,
-        email: null,
-        photo: null,
-        gender: 'woman',
-        weight: null,
-        sportTime: null,
-        dailyNorma: 1500
-    },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
-    isLoading: false,
-    error: null,
-}
+  user: {
+    name: null,
+    email: null,
+    photo: null,
+    gender: 'woman',
+    weight: null,
+    sportTime: null,
+    dailyNorma: 1500,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  isLoading: false,
+  error: null,
+};
 
-// const handlePending = (state) => {
-//     state.isLoading = true;
-//     state.error = null;
-// }
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
 
 // const handleRejected = (state, action) => {
-//     state.isLoading = false;
-//     state.error = action.payload;
-// }
+//   state.isLoading = false;
+//   state.error = action.payload;
+// };
 
-// const handleFulfilled = (state, action) => {
-//     state.user = action.payload.user;
-//     state.token = action.payload.token;
-//     state.isLoading = false;
-//     state.isLoggedIn = true;
-//     state.error = null;
-// }
+const handleFulfilled = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoading = false;
+  state.isLoggedIn = true;
+  state.error = null;
+};
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    extraReducers: (builder) => {
-        builder
-            // .addCase(register.pending, handlePending)
-    }
-    
-})
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(logIn.pending, handlePending)
+      .addCase(logIn.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+        toast.error(`Login failed: ${action.payload}`);
+      })
+      .addCase(logIn.fulfilled, handleFulfilled);
+    // .addCase(register.pending, handlePending)
+  },
+});
 
 export default authSlice.reducer;
