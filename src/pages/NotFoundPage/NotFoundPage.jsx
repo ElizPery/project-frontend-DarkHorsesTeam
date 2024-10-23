655566507af3f8d5cfca39c00716735f860b92f4
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-10-23 12:30:05 +0100

    fixed NotFoundPages

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,38 +1,16 @@
-//import { TbError404 } from 'react-icons/tb';
-//import DocumentTitle from '../../components/DocumentTitle';
-//import css from './NotFoundPage.module.css';
+import { TbError404 } from 'react-icons/tb';
+import DocumentTitle from '../../components/DocumentTitle';
+import css from './NotFoundPage.module.css';
 
-//export default function NotFoundPage() {
-//  return (
-//    <div>
-//      <DocumentTitle>Not found</DocumentTitle>
-
-   //   <div className={css.container}>
-    //    <TbError404 className={css.errorIcon} />
-    //    <h1 className={css.title}>Sorry, page not found</h1>
- //     </div>
- //   </div>
-//  );
-//}
-
-import { useState } from 'react';
-import SettingModal from '../../components/SettingModal/SettingModal.jsx';
-
-const NotFoundPage = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
-
+export default function NotFoundPage() {
   return (
     <div>
-      <button onClick={openModal}>Settings</button>
-      <SettingModal 
-        isOpen={isModalOpen} 
-        onClose={closeModal} 
-      />
+      <DocumentTitle>Not found</DocumentTitle>
+
+      <div className={css.container}>
+        <TbError404 className={css.errorIcon} />
+        <h1 className={css.title}>Sorry, page not found</h1>
+      </div>
     </div>
   );
-};
-
-export default NotFoundPage;
+}
