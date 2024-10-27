e1899149d5f1fdc6466437c1f79d1c4380f2ae29
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-10-27 17:17:23 +0200

    fixed popup position

--- src/components/DaysGeneralStats/DaysGeneralStats.jsx
+++ src/components/DaysGeneralStats/DaysGeneralStats.jsx
@@ -1,25 +1,41 @@
-import css from "./DaysGeneralStats.module.css";
+import css from './DaysGeneralStats.module.css';
 
-const DaysGeneralStats = ({ date, dailyNorm, fulfillment, servings }) => {
-    return (
-        <div className={css.container}>
-            <div className={css.allText}>
-                <p className={css.date}>{date}</p>
-                <p className={css.dailyNorm}>
-                    <span>Daily norma: </span>
-                    <span className={css.value}>{dailyNorm} L</span>
-                </p>
-                <p className={css.fulfillment}>
-                    <span>Fulfillment of the daily norm: </span>
-                    <span className={css.value}>{fulfillment}%</span>
-                </p>
-                <p className={css.servings}>
-                    <span>How many servings of water: </span>
-                    <span className={css.value}>{servings}</span>
-                </p>
-            </div>
-        </div>
-    );
+const DaysGeneralStats = ({
+  date,
+  dailyNorm,
+  fulfillment,
+  servings,
+  centerPosition,
+}) => {
+  const popupLength = 280;
+  let left = 'auto';
+  let right = 'auto';
+  if (centerPosition - popupLength / 2 < 0) {
+    left = 0;
+  }
+  if (centerPosition + popupLength / 2 > window.innerWidth) {
+    right = 0;
+  }
+
+  return (
+    <div className={css.container} style={{ left: left, right: right }}>
+      <div className={css.allText}>
+        <p className={css.date}>{date}</p>
+        <p className={css.dailyNorm}>
+          <span>Daily norma: </span>
+          <span className={css.value}>{dailyNorm} L</span>
+        </p>
+        <p className={css.fulfillment}>
+          <span>Fulfillment of the daily norm: </span>
+          <span className={css.value}>{fulfillment}%</span>
+        </p>
+        <p className={css.servings}>
+          <span>How many servings of water: </span>
+          <span className={css.value}>{servings}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default DaysGeneralStats;
