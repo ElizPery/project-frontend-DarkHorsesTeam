8921cddfc8ba33f6e66a8c70002d4944f819d6d7
biletskakatia <152877295+biletskakatia@users.noreply.github.com>
2024-10-28 13:32:44 +0200

    validation  add water

--- src/components/TodayListModal/TodayListModal.jsx
+++ src/components/TodayListModal/TodayListModal.jsx
@@ -75,10 +75,14 @@ export default function TodayListModal({
 
   const handleSave = () => {
     if (volume <= 0) {
-    toast.error('Value must be greater than 0.'); 
-    console.error('Volume must be greater than 0.');
+    toast.error('Value must be greater than 0'); 
+    console.error('Volume must be greater than 0');
     return;
-  }
+    }
+    if (volume > 5000) {
+      toast.error('Value must be less than 5000'); 
+      console.error('Value must be less than 5000');
+    }
     if (!item?._id && !isAdding) {
       console.error('Item ID is undefined.');
       return;
@@ -241,7 +245,7 @@ export default function TodayListModal({
             </h3>
             <input
               type="number"
-              value={volume > 0 ? volume : ''}
+              value={volume}
               min="0"
               onChange={e => handleVolumeChange(Number(e.target.value))}
               onBlur={() => handleVolumeChange(volume)}
